cacb8dc2486fb0b934a8f2400b25afd426fc83b6
sesughyaagba <sesughdesmond@gmail.com>
2023-10-05 10:02:11 +0100

    updated

--- Week04/E-commerce/src/App.jsx
+++ Week04/E-commerce/src/App.jsx
@@ -1,22 +1,33 @@
-
-import Header from "./Components/Header/Header";
-import EditorPick from "./Components/EditorPick";
-import FeaturedProducts from "./Components/FeaturedProducts";
-import MiddleBanner from "./Components/MiddleBanner"
-import AsianWears from "./Components/AsianWears";
+import Header from './Components/Header/Header';
+import EditorPick from './Components/EditorPick';
+import FeaturedProducts from './Components/FeaturedProducts';
+import MiddleBanner from './Components/MiddleBanner';
+import AsianWears from './Components/AsianWears';
+import FeaturedPosts from './Components/FeaturedPostsHeader';
+import FeaturedPostsHeader from './Components/FeaturedPostsHeader';
 function App() {
-
   return (
     <>
-      <div><Header /></div>
-      <div><EditorPick /></div>
-      <div><FeaturedProducts /></div>
-      <div><MiddleBanner /></div>
-      <div><AsianWears/></div>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <EditorPick />
+      </div>
+      <div>
+        <FeaturedProducts />
+      </div>
+      <div>
+        <MiddleBanner />
+      </div>
+      <div>
+        <AsianWears />
+      </div>
+      <div>
+        <FeaturedPostsHeader />
+      </div>
     </>
-  )
+  );
 }
 
 export default App;
-
-
