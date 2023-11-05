8e73b71015f2ee99ef64e99db023654aad1394bc
sesughyaagba <sesughdesmond@gmail.com>
2023-11-05 18:01:06 +0100

    Task added

--- Week04/E-commerce/src/Components/EditorPick.jsx
+++ Week04/E-commerce/src/Components/EditorPick.jsx
@@ -70,43 +70,4 @@ const EditorPick = () => {
 };
 
 export default EditorPick;
-// const EditorPick = () => {
-//   return (
-//     <section className="editor-pick">
-//       <header className="editor-heading">
-//         <h3 className="editor-title">EDITOR'S PICK</h3>
-//         <p className="editor-statement">
-//           Problems trying to resolve the conflict between
-//         </p>
-//       </header>
-//       <div className="editor-imgs">
-//         <div className="large-img">
-//           <img className="image-response" src={men} alt="men" />
-//           <button className="men-btn">MEN</button>
-//         </div>
-//         <div className="grid-imgs">
-//           <div className="women-wears">
-//             <img className="image-response" src={Women} alt="women" />
-//             <button className="women-btn">WOMEN</button>
-//           </div>
-//           <div>
-//             <div className="accessories">
-//               <img
-//                 className="image-response"
-//                 src={Acessories}
-//                 alt="accessories"
-//               />
-//               <button className="accessories-btn">ACCESSORIES</button>
-//             </div>
-//             <div className="kids-wears">
-//               <img className="image-response" src={Kids} alt="kids" />
-//               <button className="kids-btn">KIDS</button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
 
-// export default EditorPick;
